e9b01aa82325147500a7ebd6017be77ad45d5ed2
negebauer <negebauer@uc.cl>
2018-08-07 16:07:44 -0400

    feat: only set sentry on actual device

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { AppRegistry } from 'react-native'
 import { Sentry } from 'react-native-sentry'
+import DeviceInfo from 'react-native-device-info'
 import App from './src/App'
 import { name } from './app.json';
 
-process.env.NODE_ENV !== 'development'
-  && Sentry.config('https://dd6894b022894719a63a190718c2d25c@sentry.io/1250052').install()
+if (!DeviceInfo.isEmulator()) {
+  Sentry.config('https://dd6894b022894719a63a190718c2d25c@sentry.io/1250052').install()
+}
 
 AppRegistry.registerComponent(name, () => App)
