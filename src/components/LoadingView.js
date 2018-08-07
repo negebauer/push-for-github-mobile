2bf0e88e3fd0e749fdbfbeb8b338ca339967ffe5
negebauer <negebauer@uc.cl>
2018-08-07 16:14:44 -0400

    refactor: remove unused colors from LoadingView

--- src/components/LoadingView.js
+++ src/components/LoadingView.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StyleSheet, View, Text, ActivityIndicator } from 'react-native'
-import { color1, color2, color3, color4, color5 } from '../config/colors'
+import { color1, color3, color5 } from '../config/colors'
 
 export default function LoadingView({ text, size }) {
   return (
