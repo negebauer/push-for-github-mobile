071cc04c0cb01b43c7e77455ff1249a4c4437253
negebauer <negebauer@uc.cl>
2018-08-07 16:01:32 -0400

    feat: activate sentry on production env

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Sentry } from 'react-native-sentry'
 import App from './src/App'
 import { name } from './app.json';
 
-Sentry.config('https://dd6894b022894719a63a190718c2d25c@sentry.io/1250052').install()
+process.env.NODE_ENV !== 'development'
+  && Sentry.config('https://dd6894b022894719a63a190718c2d25c@sentry.io/1250052').install()
 
 AppRegistry.registerComponent(name, () => App)
