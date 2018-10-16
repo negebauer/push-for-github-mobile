2f5b659957912843247ccd00b77975be18512b30
Nicolás Gebauer <negebauerm@gmail.com>
2018-10-16 04:02:21 -0300

    chore: move sentry url to env variable (#49)

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { AppRegistry, YellowBox } from 'react-native'
 import { Sentry } from 'react-native-sentry'
 import DeviceInfo from 'react-native-device-info'
+import Config from 'react-native-config'
 import App from './src/App'
 import { name } from './app.json';
 
 if (!DeviceInfo.isEmulator()) {
-  Sentry.config('https://dd6894b022894719a63a190718c2d25c@sentry.io/1250052').install()
+  Sentry.config(Config.SENTRY_API_URL).install()
 }
 
 AppRegistry.registerComponent(name, () => App)
