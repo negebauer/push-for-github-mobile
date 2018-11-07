6f54f47bc0826d78ecba9bf0fa3935bc9608dfb8
Nicolás Gebauer <negebauerm@gmail.com>
2018-11-07 13:17:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -224,7 +224,7 @@ export default class App extends React.Component {
       [
         { text: 'Cancel', onPress: () => { }, style: 'cancel' },
         {
-          text: 'Open url',
+          text: 'Open urll',
           onPress: () => Linking.canOpenURL(url).then(canOpen => canOpen && Linking.openURL(url)),
         },
       ],
